baf95abffd812f1f3a1757419eac0e2b589a0b3c
VamshiVammi45 <vamshivammi45@gmail.com>
2024-10-22 13:05:30 +0530

    total amount added

--- src/main/java/com/shoppingmart/ecommerceClothing/BookingServices/BookingService.java
+++ src/main/java/com/shoppingmart/ecommerceClothing/BookingServices/BookingService.java
@@ -1,9 +1,12 @@
 package com.shoppingmart.ecommerceClothing.BookingServices;
-
+import java.util.*;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.shoppingmart.ecommerceClothing.productsService.ProductRepo;
+import com.shoppingmart.ecommerceClothing.productsService.Productsdto;
 
 @Service
 public class BookingService {
@@ -11,9 +14,20 @@ public class BookingService {
     @Autowired
     private BookingDao bookingDao;
 
+    @Autowired
+    private ProductRepo productRepo;
+
     public BookingDto saveBooking(BookingDto bookingDto)
     {
+        int pid=bookingDto.getProductId();
+        int quantity=bookingDto.getQuantity();
+        Optional<Productsdto> optional=productRepo.findById(pid);
+        int totalprice=optional.get().getPrice()*quantity;
+        bookingDto.setTotalPrice(totalprice);
+        int price=optional.get().getPrice();
+        bookingDto.setPrice(price);
         bookingDto.setBookingdateTime(LocalDateTime.now());
+
         return bookingDao.saveBooking(bookingDto);
         
     }
